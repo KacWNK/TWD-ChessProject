9a2bdfc0461f51aa7605d5fe2d24ae2877515526
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-01-02 23:35:54 +0100

    Add downloader

--- downloader.py
+++ downloader.py
@@ -1,2 +1,34 @@
+import browser_cookie3
+import json
+import time
 import requests
+import re
+from lxml import html
 
+BASE_URL = "https://www.chess.com/games/archive?gameOwner=my_game&gameTypes%5B0%5D=chess&gameTypes%5B1%5D=chess960&gameType=live&page="
+
+id_regex = re.compile('data\-game\-id="(\d+)"')
+cookie_jar = browser_cookie3.firefox(domain_name=".chess.com")
+game_ids = []
+json_obj = json.loads("[]")
+for i in range(1, 45):
+    r = requests.get(BASE_URL + "1", cookies=cookie_jar)
+    print(r)
+    new_ids = [match.group(1) 
+               for match in id_regex.finditer(r.text)]
+    game_ids.extend(new_ids)
+    time.sleep(0.5)
+    payload = {
+        "ids": ",".join([str(id) for id in game_ids]),
+        "types": ",".join(["game_live" for _ in range(len(game_ids))])
+    }
+    res = requests.post(
+        "https://www.chess.com/callback/game/pgn-info",
+        data = payload
+    )
+    json_obj.extend(json.loads(res.text))
+    
+with open("output.json", "w+") as f:
+    json.dump(json_obj, f)
+
+print(len(game_ids))
--- parser.py
+++ parser.py
@@ -1,14 +1,33 @@
 import pandas as pd
 import numpy as np
 from dataclasses import dataclass
+import json
+from chess_com_extractor.tcn import decode_tcn
+import chess
+import chess.pgn
+from chess import Move
+from typing import Iterable
+import re
 
 
+def generate_pgn(tcnMoves: Iterable[Move]) -> str:
+    game = chess.pgn.Game()
+    moves = decode_tcn(tcnMoves)
+    game.add_line(moves)
+    raw_pgn = game.__str__().split("\n\n")[-1]
+    return re.sub("(\d+\.) ", "\\1", raw_pgn)
+    
 
-FILE = "chessmaster2799-white-1.pgn"
+with open("./output.json", "rb") as json_file:
+    json_reader = json.load(json_file)
+    for json_game in json_reader:
 
-
-
-
+        tcnMoves = json_game["tcnMoves"]
+        board = chess.Board()
+        game = chess.pgn.Game()
+        generate_pgn(tcnMoves)
+        # Player 1 plays allwasy white
+        print(json_game["player1Name"])
 
 
 
