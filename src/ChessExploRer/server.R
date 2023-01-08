e99bd3e867a4e8dc6c67a8b60bb7b4de52dbfcb1
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-01-08 22:20:56 +0100

    Convert shinyapp into shiny semantic app

--- src/ChessExploRer/server.R
+++ src/ChessExploRer/server.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(semantic.dashboard)
+library(shiny.semantic)
 library(ggplot2)
 library(dplyr)
 library(plotly)
@@ -13,6 +13,16 @@ CREATORS <- c(
   "Kacper Wnęk"
 )
 
+dfMoveQuality <- read.csv("./resources/MoveQuality.csv")
+mapdata <- read.csv("./resources/WorldStats.csv")
+dfWinRate <- read.csv("./resources/WinRate.csv")
+dfGamesData <- read.csv("./resources/GamesData.csv")
+
+dfGamesData %>% 
+  mutate(date = as.Date(date)) %>%  
+  mutate(endHour = substring(endHour,1, nchar(endHour)-3)) %>% 
+  mutate(endHour = as.numeric(gsub(":", "\\.", endHour))) -> dfGamesData
+
 
 shinyServer(function(input, output) {
   # Change creator 
@@ -24,11 +34,156 @@ shinyServer(function(input, output) {
   }
   update_creator(1)
   
-  output$distPlot <- renderPlot({
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
+  ## MAP FOR KACPER
+  output$mapKacper <- renderPlot({
+    mapdata[mapdata$Player == "Kacper", ] %>%
+    ggplot(aes_string(x = "long", y = "lat", group = "group", fill = input$fill_var)) +
+      geom_polygon(color = "black") +
+      scale_fill_gradient(name =ifelse(input$fill_var=="WinP", "Win Ratio(%)","Average Accuracy"),
+                          low = ifelse(input$fill_var == "WinP", "#6c9d41", "orange"),
+                          high = ifelse(input$fill_var == "WinP", "#4e7838", "red"),
+                          na.value = ifelse(input$fill_var == "WinP", "#94bb48", "yellow"),
+                          trans = "log10") +
+      theme(axis.text.x = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks = element_blank(),
+            axis.title.y = element_blank(),
+            axis.title.x = element_blank(),
+            rect = element_blank(),
+            panel.grid = element_blank(),
+            plot.background = element_rect(fill='transparent', color=NA)) +
+      labs(title = paste("Player Kacper vs World"))
+  }, bg="transparent")
+  
+  ## MAP FOR KACPER
+  output$mapKrzysiek <- renderPlot({
+    mapdata[mapdata$Player == "Krzysiek", ] %>%
+    ggplot(aes_string(x = "long", y = "lat", group = "group", fill = input$fill_var)) +
+      geom_polygon(color = "black") +
+      scale_fill_gradient(name =ifelse(input$fill_var=="WinP", "Win Ratio(%)","Average Accuracy"),
+                          low = ifelse(input$fill_var == "WinP", "#6c9d41", "orange"),
+                          high = ifelse(input$fill_var == "WinP", "#4e7838", "red"),
+                          na.value = ifelse(input$fill_var == "WinP", "#94bb48", "yellow"),
+                          trans = "log10") +
+      theme(axis.text.x = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks = element_blank(),
+            axis.title.y = element_blank(),
+            axis.title.x = element_blank(),
+            rect = element_blank(),
+            panel.grid = element_blank(),
+            plot.background = element_rect(fill='transparent', color=NA)) +
+      labs(title = "Player Krzysiek vs World")
+  }, bg="transparent")
+
+  
+  output$eloPlot <- renderPlot({
+    dfGamesData %>% filter(date >= input$date_from,
+                           date <= input$date_to,
+                           timeControl %in% input$typeElo,
+                           player %in% input$playerElo) -> df2
+    
+    ggplot(data = df2, aes(x = date, y = yourElo))+
+      geom_point()+
+      geom_line()+
+      labs(x = "Date", y = "Rating points")+
+      theme(
+        panel.background = element_rect(fill='transparent'), 
+        plot.background = element_rect(fill='transparent', color=NA), 
+      )
+  }, bg="transparent")
+  
+  output$moveQualityPlot <- renderPlotly({
+    dfMoveQuality %>%
+      filter(Type %in% input$typeMoveQuality,
+             Color %in% input$colorMoveQuality,
+             Player %in% input$playerMoveQuality) %>% 
+      mutate(Move = fct_reorder(Move, Procent, .desc = FALSE))->dfMoveQualityPlot
+    
+    ggplot(data = dfMoveQualityPlot, aes(x = Procent, y = Move, fill = MoveColor))+
+      geom_col()+
+      labs(x = "Percent", y = "Move type")+
+      scale_fill_manual(values = unique(dfMoveQualityPlot$MoveColor))+ 
+      scale_x_continuous(expand = c(0,0))+
+      theme(legend.position="none")+
+      theme(
+        panel.background = element_rect(fill='transparent'), 
+        plot.background = element_rect(fill='transparent', color=NA)
+      )
+    
   })
+  
+  output$winRatePlot <- renderPlot({
+    
+    dfWinRate %>% 
+      filter(Type %in% input$typeWinRate,
+             Player %in% input$playerWinRate)-> dfWinRatePlot
+    
+    ggplot(dfWinRatePlot, aes(x = "", y = Matches, fill = Result)) +
+      geom_col(width = 0.5) +
+      geom_text(aes(label = paste(Percentages, "%")), position = position_stack(vjust = 0.5)) +
+      scale_fill_manual(values = dfWinRatePlot$Colors) +
+      labs(x = "", y = "Number of Matches") +
+      theme_void()+
+      theme(legend.position="bottom")+
+      coord_flip()+
+      theme(
+        panel.background = element_rect(fill='transparent'), 
+        plot.background = element_rect(fill='transparent', color=NA)
+      )
+  }, bg="transparent")
+  
+  
+  output$timeLagElo <- renderUI({
+    dfGamesData %>% filter(player %in% input$playerElo, timeControl %in% input$typeElo) -> df3
+    tagList(
+      tags$div(
+        tags$div(HTML("From")),
+        date_input(
+          "date_from", 
+          value = min(df3$date), 
+          min = min(df3$date),
+          max = max(df3$date))
+      ),
+      br(),
+      tags$div(
+        tags$div(HTML("To")),
+        date_input(
+          "date_to", 
+          value = max(df3$date), 
+          min = min(df3$date),
+          max = max(df3$date))
+      )
+    )
+  })
+  
+  output$densPlot <- renderPlot({
+    
+    ggplot(data = dfGamesData %>% filter(player == "Kacper"), aes(x = endHour))+
+      geom_density(fill = "#96bc4b")+
+      labs(x = "Time", y = "Density", title = "Kacper")+
+      scale_x_continuous(expand = c(0,0))+
+      scale_y_continuous(expand = c(0,0))+
+      theme(
+        panel.background = element_rect(fill='transparent'), 
+        plot.background = element_rect(fill='transparent', color=NA), 
+      )-> p1
+    ggplot(data = dfGamesData %>% filter(player == "Krzysiek"), aes(x = endHour))+
+      geom_density(fill = "#1bada6")+
+      labs(x = "Time", y = "Density", title = "Krzysiek")+
+      scale_x_continuous(expand = c(0,0))+
+      scale_y_continuous(expand = c(0,0))+
+      theme(
+        panel.background = element_rect(fill='transparent'), 
+        plot.background = element_rect(fill='transparent', color=NA), 
+      )->p2
+    grid.arrange(p1,p2, ncol =2)
+    
+  }, bg="transparent")
+  
+  
+  output$selected_gif <- renderImage({
+    list(src = input$gif)
+  }, deleteFile = FALSE)
+  
 })
--- src/ChessExploRer/ui.R
+++ src/ChessExploRer/ui.R
@@ -5,16 +5,22 @@ library(semantic.dashboard)
 # options(semantic.themes = TRUE)
 # options(shiny.custom.semantic = "www/")
 
+source("server.R")
 
+### HEADER
 header <- dashboardHeader(
   includeCSS("./www/header.css"),
   class = "dsHeader",
   logo_path = "logo.png",
   logo_align = "center",
   title = "Chess ExploRer",
-  right = textOutput("creator"),
+  right = div(
+    id = "creator-container",
+    textOutput("creator"),
+  )
 )
 
+### SIDEBAR
 sidebar <- dashboardSidebar(size = "thin", color = "brown",
   sidebarMenu(
     includeCSS("./www/sidebar.css"),
@@ -26,14 +32,168 @@ sidebar <- dashboardSidebar(size = "thin", color = "brown",
   )
 )
 
+### TABS
+
+##### HOME TAB TODO
+homeTab <- semanticPage(
+  title = "My page",
+  div(class = "flex center ",
+      div(class = "ui button", 
+          icon("user"),  
+          "Icon button"
+      )
+  )
+)
+
+##### MAP TAB
+mapTab <- semanticPage(
+  title = "Map",
+  div(class = "ui grid",
+    div(class = "row",
+      multiple_radio(
+        "fill_var", "Select type: ",
+        c("Win Ratio"="WinP","Average Accuracy"= "Accuracy"),
+        position = "inline"
+      )
+    ),
+    div(class = "two column row",
+        div(class = "column",
+            plotOutput(outputId = "mapKacper")
+        ),
+        div(class = "column", 
+            plotOutput(outputId = "mapKrzysiek")
+        )
+    )
+  )
+)
+
+###### GAMES TAB
+gamesTab <- semanticPage(
+  tilte = "Games",
+  div(class = "ui grid",
+      div(class = "row",
+          selectInput("gif", "Select a gif:", 
+                      choices = c("Immortal game- Kacper(white)" = "./resources/KW_immortalGame.gif",
+                                  "First game- Kacper(white)" = "./resources/KacperPierwszaPartia.gif",
+                                  "Immortal game- Krzysiek"="./resources/gigachad-chad.gif",
+                                  "First game- Krzysiek(white)"="./resources/Krzysiu_pierwszaPartia.gif")),
+      ),
+      div(class = "row",
+          imageOutput("selected_gif")
+      )
+  )
+)
+
+###### COMPETITION TAB
+compTabRow1 <- div(
+  class = "row clear-bg",
+  sidebar_layout(
+    sidebar_panel(
+      multiple_radio("typeWinRate",
+                   "Choose time control",
+                   selected = unique(dfWinRate$Type)[1],
+                   choices = c("Bullet", "Blitz", "Rapid"),
+                   choices_value = unique(dfWinRate$Type)
+      ),
+      br(), br(),
+      multiple_radio("playerWinRate",
+                   "Select a player",
+                   selected = unique(dfWinRate$Player)[1],
+                   choices = unique(dfWinRate$Player)
+      ),
+      width = 2
+    ),
+    main_panel(
+      plotOutput("winRatePlot"),
+      width = 10
+    ),
+    container_style = "padding-left: 20px"
+  )
+)
+
+compTabRow2 <- div(
+  class = "row clear-bg",
+  sidebar_layout(
+    sidebar_panel(
+      multiple_radio("colorMoveQuality",
+                     "Choose color of pieces",
+                     selected = unique(dfMoveQuality$Color)[1],
+                     choices = c("White", "Black", "White and black"),
+                     choices_value = unique(dfMoveQuality$Color)
+      ),
+      br(), br(),
+      multiple_radio("playerMoveQuality",
+                     "Select a player",
+                     selected = unique(dfMoveQuality$Player)[1],
+                     choices = unique(dfMoveQuality$Player)
+      ),
+      br(), br(),
+      multiple_radio("typeMoveQuality",
+                     "Choose time control",
+                     selected = unique(dfMoveQuality$Type)[1],
+                     choices = c("Bullet", "Blitz", "Rapid"),
+                     choices_value = unique(dfMoveQuality$Type)
+      ),
+      width = 2
+    ),
+    main_panel(
+      plotlyOutput("moveQualityPlot"),
+      width = 10
+    ),
+    container_style = "padding-left: 20px"
+  ) 
+)
+
+compTabRow3 <- div(
+  class = "row clear-bg",
+  sidebar_layout(
+    sidebar_panel(
+      multiple_radio("playerElo",
+                   "Select a player",
+                   selected = unique(dfGamesData$player)[1],
+                   choices = unique(dfGamesData$player)
+      ),
+      br(), br(),
+      multiple_radio("typeElo",
+                   "Choose time control",
+                   selected = unique(dfGamesData$timeControl)[1],
+                   choices = unique(dfGamesData$timeControl)
+      ),
+      br(), br(),
+      uiOutput("timeLagElo"),
+      width = 2
+    ),
+    main_panel(
+      plotOutput("eloPlot"),
+      width = 10
+    ),
+    container_style = "padding-left: 20px"
+  ) 
+)
+
+compTab <- semanticPage(
+  title = "Competition page",
+  div(class = "ui grid",
+      compTabRow1,
+      compTabRow2,
+      compTabRow3,
+      div(class="row",
+          plotOutput("densPlot")
+      )
+  )
+)
+
+### BODY 
 body <- dashboardBody(class = "dsBody", tabItems(
   includeCSS("./www/body.css"),
-  tabItem(tabName = "home", p("home")),
-  tabItem(tabName = "comp", p("comp")),
-  tabItem(tabName = "map", p("map")),
-  tabItem(tabName = "games", p("games"))
+  tabItem(tabName = "home", homeTab),
+  tabItem(tabName = "comp", compTab),
+  tabItem(tabName = "map", mapTab),
+  tabItem(tabName = "games", gamesTab)
 ))
 
+
+### RUN APP
 shinyUI(dashboardPage(
   title = 'ChessExploRer',
   header, sidebar, body,
